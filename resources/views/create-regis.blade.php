2daa240ae30424925088a6a30baf61ebe8497e44
ApoloLB <apololbastos@gmail.com>
2026-09-04 17:27:47 -0300

    first route and controller

---
+++ resources/views/create-regis.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <section>
+        <div class="title"></div>
+
+        {{-- REGISTRATIONS START --}}
+        <div class="registrations"> 
+            <div class="registratrionsTitle"></div>
+            <div class="registratrionsInput">
+
+                <div class="codeInput"></div>
+                <div class="descriptionInput"></div>
+                
+            </div>
+        </div>
+
+        <div class="display"></div>
+    </section>
+</body>
+</html>
