f55a4731a24f5037f7c61dd0685a9dd647ce42f6
ApoloLB <157991256+ApoloLB@users.noreply.github.com>
2026-09-25 16:27:07 -0300

    Update routes/web.php
    Co-authored-by: Gustavo Camalionti <54083715+gustavocamalionti@users.noreply.github.com>

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,6 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    // User::create([ 'name' => 'Apolobastos',]);
     return view('welcome');
 });
 
